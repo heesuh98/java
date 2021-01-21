f08e3fa9c96afa9726dfc97185f4ec95329825df
Julia Cho <73007888+heesuh98@users.noreply.github.com>
2021-01-21 23:29:58 +0900

    Add files via upload

---
+++ Stringapp.java
@@ -0,0 +1,22 @@
+package data_and_operation;
+
+public class Stringapp {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World"); //String:character들이 모여있는 것
+		//System.out.println('Hello World'); //Character:한글자를 표현하는 데이터타입
+		System.out.println("H");
+		
+		System.out.println("Hello"
+				+ "World");
+		
+	//New line
+		System.out.println("Hello \nWorld");
+		
+	//Escape
+		System.out.println("Hello \"World\"");
+	
+	}
+
+}
